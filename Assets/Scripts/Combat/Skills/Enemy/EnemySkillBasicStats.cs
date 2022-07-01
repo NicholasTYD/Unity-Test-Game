5efa59853f32c66a8d4a8dd03be9aa3cdfa8d82d
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-07-01 15:45:19 +0800

    test

--- Assets/Scripts/Combat/Skills/Enemy/EnemySkillBasicStats.cs
+++ Assets/Scripts/Combat/Skills/Enemy/EnemySkillBasicStats.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemySkillBasicStats", menuName = "ScriptableObjects/Enemy/EnemySkillBasicStats")]
 public class EnemySkillBasicStats : ScriptableObject
-{ 
-    public new string name;
-    public float DamageMultiplier;
-    public float DurationPerFrame;
+{
+    [SerializeField] public new string name;
+    [SerializeField] public float DamageMultiplier;
+    [SerializeField] public float DurationPerFrame;
 
     // How long the skill lasts
-    public float SkillDuration;
-    public float SkillCooldown;
+    [SerializeField] public float SkillDuration;
+    [SerializeField] public float MinSkillCooldown;
+    [SerializeField] public float MaxSkillCooldown;
 
     // How long afterwards which enemy cannot use any attacks.
-    public float minAttackLockoutDuration;
-    public float maxAttackLockoutDuration;
+    [SerializeField] public float minAttackLockoutDuration;
+    [SerializeField] public float maxAttackLockoutDuration;
 }
