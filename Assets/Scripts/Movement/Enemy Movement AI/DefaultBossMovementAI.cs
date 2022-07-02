904f294ee051d000c5c1fb6445998ea8f9c6bc66
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-07-02 23:00:01 +0800

    Deleted redundant classes for real

--- Assets/Scripts/Movement/Enemy Movement AI/DefaultBossMovementAI.cs
+++ Assets/Scripts/Movement/Enemy Movement AI/DefaultBossMovementAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArcherMovementAI : NormalEnemyMovement
+public class DefaultBossMovementAI : BossMovement
 {
-
+    
 }
