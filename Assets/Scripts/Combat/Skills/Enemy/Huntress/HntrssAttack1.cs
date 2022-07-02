1ef14fd3dbfcd6fa543fb8f23db33c9f7b20da1c
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-07-02 23:40:40 +0800

    Cleaned up huntress code

--- Assets/Scripts/Combat/Skills/Enemy/Huntress/HntrssAttack1.cs
+++ Assets/Scripts/Combat/Skills/Enemy/Huntress/HntrssAttack1.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class HntrssAttack1 : DefaultEnemySkill
 {
-    [SerializeField] EnemySkill HntrssAttack2;
 
-    public override void ExecuteSkill(EnemyMain enemy, PlayerMain player)
-    {
-        base.ExecuteSkill(enemy, player);
-        StartCoroutine(executeAttack2(enemy, player));
-    }
-
-    IEnumerator executeAttack2(EnemyMain enemy, PlayerMain player)
-    {
-        yield return new WaitForSeconds(skillCooldownTimer);
-        HntrssAttack2.ExecuteSkill(enemy, player);
-    }
 }
--- Assets/Scripts/Combat/Skills/Enemy/Huntress/HntrssAttack2.cs
+++ Assets/Scripts/Combat/Skills/Enemy/Huntress/HntrssAttack2.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class HntrssAttack2 : EnemySkill
 {
-    [SerializeField] EnemySkill HntrssAttack3;
-
     public override void ExecuteSkill(EnemyMain enemy, PlayerMain player)
     {
         enemyMovement.FaceTowards(player.transform.position);
         base.ExecuteSkill(enemy, player);
-        StartCoroutine(executeAttack2(enemy, player));
-    }
-
-    IEnumerator executeAttack2(EnemyMain enemy, PlayerMain player)
-    {
-        yield return new WaitForSeconds(skillCooldownTimer);
-        HntrssAttack3.ExecuteSkill(enemy, player);
     }
 }
