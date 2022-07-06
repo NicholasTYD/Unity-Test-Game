873024706a0c0d186bb9d8cdc6faf17a75b4a0e0
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-07-06 17:32:32 +0800

    Completed meteor

---
+++ Assets/Scripts/Combat/Skills/Enemy/Samurai/Meteor/SamuraiMeteor.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiMeteor : MonoBehaviour
+{
+    [SerializeField] GameObject warningCircle;
+    [SerializeField] GameObject fireball;
+    [SerializeField] GameObject explosion;
+    [SerializeField] GameObject shockwavePrefab;
+    [SerializeField] List<Vector2> shockwaveDirections;
+
+    CircleCollider2D damageHurtbox;
+    
+    float warningTime = 1;
+    float fireballTravelTime = 0.5f;
+    float circleColliderActiveTime = 0.5f;
+    float destroyAfterSecondsPastExplosion = 2;
+
+    float meteorDamage = 10;
+    float shockwaveDamage = 10;
+    float shockwaveSpeed = 10;
+    float shockwaveLifetime = 10;
+
+    private void Start()
+    {
+        this.damageHurtbox = this.GetComponent<CircleCollider2D>();
+        StartCoroutine(this.executeAttack());
+    }
+
+    public void SetStats(float meteorDamage, float shockwaveDamage, float shockwaveSpeed, float shockwaveLifetime)
+    {
+        this.meteorDamage = meteorDamage;
+        this.shockwaveDamage = shockwaveDamage;
+        this.shockwaveSpeed = shockwaveSpeed;
+        this.shockwaveLifetime = shockwaveLifetime;
+    }
+
+    protected virtual void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            CombatMechanics.Instance.DealDamageTo(collision.gameObject, meteorDamage);
+        }
+    }
+
+    protected void destroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator executeAttack()
+    {
+        yield return new WaitForSeconds(warningTime);
+        Debug.Log("test1");
+
+        fireball.SetActive(true);
+
+        yield return new WaitForSeconds(fireballTravelTime);
+        warningCircle.SetActive(false);
+        fireball.SetActive(false);
+        explosion.SetActive(true);
+        damageHurtbox.enabled = true;
+        foreach (Vector2 dir in shockwaveDirections)
+        {
+            CombatMechanics.Instance.InstantiateProjectile(shockwavePrefab, this.transform.position, dir,
+                shockwaveDamage, shockwaveSpeed, shockwaveLifetime);
+        }
+
+        yield return new WaitForSeconds(circleColliderActiveTime);
+        damageHurtbox.enabled = false;
+        explosion.SetActive(false);
+
+        yield return new WaitForSeconds(destroyAfterSecondsPastExplosion - circleColliderActiveTime);
+        destroySelf();
+    }
+}
