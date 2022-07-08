a0bb624e32fe81dffac3e5d3ddcf2aada60117ed
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-07-08 23:39:51 +0800

    Optimised sniper

--- Assets/Scripts/Gameplay/Combat/Skills/Enemy/Sniper/SniperAttack.cs
+++ Assets/Scripts/Gameplay/Combat/Skills/Enemy/Sniper/SniperAttack.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SniperAttack : EnemySkill
-{
-    [SerializeField] float tracerDelay;
+{ 
     Vector2 laserScale;
 
     protected override void Start()
@@ -13,19 +12,8 @@ public class SniperAttack : EnemySkill
         laserScale = enemyCombat.EnemyProjectileSpawnPoint.localScale;
     }
 
-    public override void ExecuteSkill(EnemyMain enemy, PlayerMain player)
+    public void AdjustTracer()
     {
-        enemyMovement.FaceTowards(player.transform.position);
-        base.ExecuteSkill(enemy, player);
-        StartCoroutine(adjustTracer(enemy, player));
-    }
-
-    IEnumerator adjustTracer(EnemyMain enemy, PlayerMain player)
-    {
-        // The small 0.01f delay is to prevent occasional instances where the beam appears to
-        // teleport due to frame update times.
-        yield return new WaitForSeconds(tracerDelay);
-
         // Adjust rotation of beam
         Vector2 laserRotation = getRangedAimDirection();
         Quaternion laserQuaternion = General.Instance.ConvertRotationToQuaternion(laserRotation);
